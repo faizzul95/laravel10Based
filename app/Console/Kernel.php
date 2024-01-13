6ca8f1b5d39c6f43c70a37428c8e6d255fbc8cc7
Fahmy Izwan <mfahmyizwan@gmail.com>
2024-01-13 12:38:35 +0800

    remove vendor public

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,46 +15,52 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        #--------------------------------------------------------------------
-        # The following command will delete all records telescope created over 18 hours ago
-        #--------------------------------------------------------------------
-        $schedule->command('telescope:prune --hours=18')->runInBackground()->evenInMaintenanceMode()->dailyAt('01:15');
 
-        #--------------------------------------------------------------------
-        # The following command will monitor backup health and clean up old/unhealthy backups.
-        #--------------------------------------------------------------------
-        $schedule->command('backup:monitor')->runInBackground()->evenInMaintenanceMode()->dailyAt('03:00');
-        $schedule->command('backup:clean')->runInBackground()->evenInMaintenanceMode()->dailyAt('05:00');
+        /**
+         * Only run in production environment
+         */
+        if (config('app.env') == 'production') {
+            #--------------------------------------------------------------------
+            # The following command will delete all records telescope created over 18 hours ago
+            #--------------------------------------------------------------------
+            $schedule->command('telescope:prune --hours=18')->name('telescope-prune')->withoutOverlapping()->evenInMaintenanceMode()->dailyAt('01:15');
 
-        #--------------------------------------------------------------------
-        # The following command will back up a database daily.
-        #--------------------------------------------------------------------
-        $schedule->command('backup:database')->runInBackground()->evenInMaintenanceMode()->dailyAt('12:01')->appendOutputTo(storage_path('logs/backup_database.log'));
+            #--------------------------------------------------------------------
+            # The following command will monitor backup health and clean up old/unhealthy backups.
+            #--------------------------------------------------------------------
+            $schedule->command('backup:monitor')->name('backup-moniter')->withoutOverlapping(500)->evenInMaintenanceMode()->dailyAt('03:00');
+            $schedule->command('backup:clean')->name('backup-clean')->withoutOverlapping(500)->evenInMaintenanceMode()->dailyAt('05:00');
 
-        #--------------------------------------------------------------------
-        # The following command will back up a filesystem on a monthly basis.
-        #--------------------------------------------------------------------
-        $schedule->command('backup:filesystem')->runInBackground()->evenInMaintenanceMode()->monthly()->appendOutputTo(storage_path('logs/backup_filesystem.log'));
+            #--------------------------------------------------------------------
+            # The following command will back up a database daily.
+            #--------------------------------------------------------------------
+            $schedule->command('backup:database')->name('backup-database')->withoutOverlapping(500)->evenInMaintenanceMode()->dailyAt('12:01')->appendOutputTo(storage_path('logs/backup_database.log'));
 
-        #--------------------------------------------------------------------
-        # The following command will run a full backup twice per month.
-        #--------------------------------------------------------------------
-        $schedule->command('backup:run')
-            ->twiceMonthly(1, 16, '04:30')  // Run the task monthly on the 1st and 16th at 04:30
-            ->runInBackground()
-            ->evenInMaintenanceMode()
-            ->before(function () {
-                // Artisan::call('down');  // Put the application into maintenance mode
-                Artisan::call('down', [
-                    '--secret' => "ABC0189031045KEY", // use as a token to bypass maintenance
-                    '--refresh' => 60,
-                    '--retry' => 7200
-                ]);
-            })
-            ->after(function () {
-                Artisan::call('up'); // Bring the application out of maintenance mode
-            })
-            ->appendOutputTo(storage_path('logs/backup_full.log'));
+            #--------------------------------------------------------------------
+            # The following command will back up a filesystem on a monthly basis.
+            #--------------------------------------------------------------------
+            $schedule->command('backup:filesystem')->evenInMaintenanceMode()->monthly()->appendOutputTo(storage_path('logs/backup_filesystem.log'));
+
+            #--------------------------------------------------------------------
+            # The following command will run a full backup twice per month.
+            #--------------------------------------------------------------------
+            $schedule->command('backup:run')
+                ->twiceMonthly(1, 16, '04:30')  // Run the task monthly on the 1st and 16th at 04:30
+                ->name('backup-full')
+                ->evenInMaintenanceMode()
+                ->before(function () {
+                    // Artisan::call('down');  // Put the application into maintenance mode
+                    Artisan::call('down', [
+                        '--secret' => "ABC0189031045KEY", // use as a token to bypass maintenance
+                        '--refresh' => 60,
+                        '--retry' => 7200
+                    ]);
+                })
+                ->after(function () {
+                    Artisan::call('up'); // Bring the application out of maintenance mode
+                })
+                ->appendOutputTo(storage_path('logs/backup_full.log'));
+        }
     }
 
     /**
